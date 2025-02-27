2cf3fe8546479c94ea0583a27b8d310384ad29b8
Nico <nicobuquetcebada@gmail.com>
2025-02-27 17:49:06 +0100

    test

--- mileston2/pipex/pipex.h
+++ mileston2/pipex/pipex.h
@@ -3,16 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nico <nico@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nbuquet- <nbuquet-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:51:43 by nico              #+#    #+#             */
-/*   Updated: 2024/12/14 21:53:32 by nico             ###   ########.fr       */
+/*   Updated: 2025/02/27 11:07:52 by nbuquet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 # include <sys/wait.h>
 
 #endif
